c41f495934f1bf7c4582d53cc1a3b9b63948aaf8
Priyadarshan <peedi@vaayujeet.com>
2024-11-30 20:34:24 +0530

    add requied .env files

--- src/encore/correlator/asgi.py
+++ src/encore/correlator/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for correlator project.
+ASGI config for encore project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- src/encore/correlator/wsgi.py
+++ src/encore/correlator/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for correlator project.
+WSGI config for encore project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
